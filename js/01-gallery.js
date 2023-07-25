5f6c8250041643eff58908e8e393f5a90e67b819
Lana2408 <119637611+Lana2408@users.noreply.github.com>
2023-07-25 01:23:00 +0100

    added HTML

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const containerGallery = document.querySelector(".gallery");
+const markup = galleryItems.map(({ preview, original, description }) => `
+ <li class = "gallery__item"> <a class="gallery__link" href="${original}">
+   <img src="${preview}" data-source="${original}" class = "gallery__image" alt="${description}" loading="lazy" width="300">
+   </a>
+ </li>
+ `
+   )
+   .join("");
+   containerGallery.insertAdjacentHTML("beforeend", markup);
 console.log(galleryItems);
+
+
