e10ec3092926302d67d2f1ef15e9730d80f5fea5
Lana2408 <119637611+Lana2408@users.noreply.github.com>
2023-07-25 20:18:27 +0100

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,18 +3,29 @@ import { galleryItems } from './gallery-items.js';
 
 
 
-console.log(galleryItems);
+//console.log(galleryItems);
 
 
-
-
-const instance = basicLightbox.create(`<img src="" width="800" height="600">`, {
-    onShow: () => {
-      document.addEventListener("keydown", onEscDown);
-    },
-    onClose: () => {
-      document.removeEventListener("keydown", onEscDown);
-    },
+const containerGallery = document.querySelector(".gallery");
+const markup = galleryItems.map(({ preview, original, description }) => `
+ <li class = "gallery__item"> <a class="gallery__link" href="${original}">
+   <img src="${preview}" class = "gallery__image" alt="${description}">
+   </a>
+ </li>
+ `
+   )
+   .join("");
+   containerGallery.insertAdjacentHTML("beforeend", markup);
+   
+   const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt", 
+    captionPosition: "bottom", 
+    captionDelay: 250,
   });
-  
-  
+
+   
+
+
+
+
